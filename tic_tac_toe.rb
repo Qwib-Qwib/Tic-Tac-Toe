00fdc7884320ff629524792f0a0bc8e955445680
Qwib-Qwib <jp93rom@orange.fr>
2022-07-25 17:37:45 +0200

    Separate the various legality checks into more compact methods, remove any reference to symbols from the Player class, implement a method to send move data to the Board for update and a few methods to update the board, add a few lines of code at the end to test the current implementation.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,14 +4,27 @@
 class Board
   SEPARATOR = '   ---|---|---'.freeze
   COLUMNS_LABELS = '    A   B   C '.freeze
+  PLAYER1_MARK = ' X '.freeze
+  PLAYER2_MARK = ' O '.freeze
 
-  def initialize
-    @line1 = ['1     ', '|', '   ', '|', '   ']
-    @line2 = ['2     ', '|', '   ', '|', '   ']
-    @line3 = ['3     ', '|', '   ', '|', '   ']
+  @line1 = ['1', '  ', '   ', '|', '   ', '|', '   ']
+  @line2 = ['2', '  ', '   ', '|', '   ', '|', '   ']
+  @line3 = ['3', '  ', '   ', '|', '   ', '|', '   ']
+
+  @move_counter = 0
+
+  # This syntax is mandatory when using class instance variables.
+  class << self
+    attr_accessor :move_counter, :line1, :line2, :line3
   end
 
-  def print_board
+  def initialize
+    @line1 = ['1', '  ', '   ', '|', '   ', '|', '   ']
+    @line2 = ['2', '  ', '   ', '|', '   ', '|', '   ']
+    @line3 = ['3', '  ', '   ', '|', '   ', '|', '   ']
+  end
+
+  def self.print_board
     puts COLUMNS_LABELS
     puts ''
     puts @line1.join('')
@@ -19,6 +32,51 @@ class Board
     puts @line2.join('')
     puts SEPARATOR
     puts @line3.join('')
+  end
+
+  def self.register_latest_move(move)
+    case move[1]
+    when '1'
+      edit_line1(move[0])
+    when '2'
+      edit_line2(move[0])
+    when '3'
+      edit_line3(move[0])
+    end
+    Board.move_counter += 1
+  end
+
+  def self.edit_line1(column)
+    case column
+    when 'A'
+      line1[2] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'B'
+      line1[4] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'C'
+      line1[6] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    end
+  end
+
+  def self.edit_line2(column)
+    case column
+    when 'A'
+      line2[2] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'B'
+      line2[4] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'C'
+      line2[6] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    end
+  end
+
+  def self.edit_line3(column)
+    case column
+    when 'A'
+      line3[2] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'B'
+      line3[4] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    when 'C'
+      line3[6] = Board.move_counter.even? ? PLAYER1_MARK : PLAYER2_MARK
+    end
   end
 end
 
@@ -32,30 +90,52 @@ class Player
     attr_accessor :shared_tiles
   end
 
-  def initialize(name, symbol)
+  def initialize(name)
     @name = name
-    @symbol = symbol
     @tiles = []
   end
 
   def select_tile
     puts "#{@name}, choose an empty tile by typing its identifier in a column-row format (for example, A1 for top left)."
     answer = gets.upcase.chomp
-    check_legality(answer)
+    answer = check_legality(answer)
+    send_move_to_board(Board, answer)
   end
 
   private
 
   def check_legality(move)
+    check_tile_occupied(move)
+    check_valid_input(move)
+    @tiles.push(move)
+    Player.shared_tiles.push(move)
+    move
+  end
+
+  def check_tile_occupied(move)
     until Player.shared_tiles.include?(move) == false
       puts 'This tile has already been selected! Please choose another one.'
       move = gets.upcase.chomp
     end
+  end
+
+  def check_valid_input(move)
     until VALID_ANSWERS.include?(move)
       puts "Your answer isn't valid, please use the column-row notation (i.e. A1, B3, C2...)"
       move = gets.upcase.chomp
     end
-    @tiles.push(move)
-    Player.shared_tiles.push(move)
+  end
+
+  protected
+
+  def send_move_to_board(target, move)
+    target.register_latest_move(move)
   end
 end
+
+player1 = Player.new('Juju')
+player2 = Player.new('Jojo')
+player1.select_tile
+Board.print_board
+player2.select_tile
+Board.print_board
